bcb1dd13cd3f611909fc7d613ec6009864f85e19
Aleksandra <uxaleksandra@gmail.com>
2017-02-14 20:58:08 +0100

    initial

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// plik scripts.js
+
+var name = prompt('Wpisz swoje imię');
+//alert('Witaj, ' + name);
+/*
+  komentarz blokowy
+  console.log(‘Zakomentowana się nie wywołam’);
+*/
+//kolejny komentarz
